dbb6170ff1a9ec92d871d603c42ae177d417c977
UNKNOWN <119309033+RAJ8664@users.noreply.github.com>
2023-06-13 17:12:43 +0545

    set_matrices_to_zero.java

--- Arrays/Medium/set_matrices_to_zero.java
+++ Arrays/Medium/set_matrices_to_zero.java
@@ -71,4 +71,50 @@ public class set_matrices_to_zero {
         //TC=O(n*m);
         //SC=O(1);
     }
+    
+    
+     /*   GFG problem;
+    *Given a matrix of  size n x m. Your task is to make Zeroes,
+    *that means in whole matrix when you find a zero,
+    *convert its upper, lower, left, and right value to zero and make that element
+    *the sum of the upper, lower, left and right value.
+    *Do the following tasks according to the initial matrix.
+    * 
+    * problem link=https://practice.geeksforgeeks.org/problems/make-zeroes4042/1?utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=make-zeroes
+    * */
+
+    public static void gfg(int[][] matrix){
+        int n=matrix.length;
+        int m=matrix[0].length;
+        int[][] arr=new int[n][m];
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                arr[i][j]=matrix[i][j];
+            }
+        }
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                int sum=0;
+                if(arr[i][j]==0){
+                    if(i-1>=0){
+                        sum+=arr[i-1][j];
+                        matrix[i-1][j]=0;
+                    }
+                    if(i+1<n){
+                        sum+=arr[i+1][j];
+                        matrix[i+1][j]=0;
+                    }
+                    if(j-1>=0){
+                        sum+=arr[i][j-1];
+                        matrix[i][j-1]=0;
+                    }
+                    if(j+1<m){
+                        sum+=arr[i][j+1];
+                        matrix[i][j+1]=0;
+                    }
+                    matrix[i][j]=sum;
+                }
+            }
+        }
+    } 
 }
