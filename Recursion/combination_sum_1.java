ecc2aaa11a7847b2e8aa0cdef52e4091fc7706e4
UNKNOWN <119309033+RAJ8664@users.noreply.github.com>
2023-06-20 17:09:31 +0545

    combination_sum_1.java

--- Recursion/combination_sum_1.java
+++ Recursion/combination_sum_1.java
@@ -3,7 +3,7 @@ public class combination_sum_1 {
     public static void main(String[] args) {
         /*
         * Given an array of distinct integers and a target,
-        * you have to return the list of all unique combinations where the chosen numbers sum to target.
+        * you have to return the list of all combinations where the chosen numbers sum to target.
         * You may return the combinations in any order
         * */
 
