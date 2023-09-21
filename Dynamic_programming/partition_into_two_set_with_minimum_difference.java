89bbae3d3809c21e52bad3cff3f43cac43fc093b
UNKNOWN <119309033+RAJ8664@users.noreply.github.com>
2023-09-21 16:33:22 +0530

    partition_into_two_set_with_minimum_difference.java

--- Dynamic_programming/partition_into_two_set_with_minimum_difference.java
+++ Dynamic_programming/partition_into_two_set_with_minimum_difference.java
@@ -1,5 +1,4 @@
 package Dynamic_programming;
-
 import java.util.Arrays;
 import java.util.Scanner;
 
@@ -13,23 +12,23 @@ public class partition_into_two_set_with_minimum_difference {
             arr[i] = sc.nextInt();
             sum += arr[i];
         }
-        int dp[][] = new int[n + 1][sum + 1];
-        for(int temp[] : dp){
-            Arrays.fill(temp,-1);
+        boolean dp[][] = new boolean[n + 1][sum + 1];
+        for(boolean temp[] : dp){
+            Arrays.fill(temp,false);
         }
         int min = Integer.MAX_VALUE;
         for(int i = 0; i <= sum; i++){
-            if(solve(n - 1, arr, i, dp) == 1){
+            if(solve_tabu(arr, i, dp)){
                 min = Math.min(min, Math.abs(i - (sum - i)));
             }
         }
         System.out.println(min);
     }
 
-
     //bruteforce;
     //may not work for negative Integers;
     //check the input;
+    //Memoization;
     public static int solve(int ind , int arr[],int target, int dp[][]){
         if(target == 0){
             return 1;
@@ -52,5 +51,30 @@ public class partition_into_two_set_with_minimum_difference {
 
         }
         return dp[ind][target] = (take | not_take);
+        //TC = O(n * target);
+        //SC = O(n * target) + O(n);
+    }
+
+    //Tabulation;
+    public static boolean solve_tabu(int arr[],int target,boolean dp[][]){
+        for(int i = 0; i < arr.length; i++){
+            dp[i][0] = true;
+        }
+        if(arr[0] <= target){
+            dp[0][arr[0]] = true;
+        }
+        for(int ind = 1; ind < arr.length; ind++){
+            for(int k = 1; k <= target; k++){
+                boolean not_take = dp[ind - 1][k];
+                boolean take = false;
+                if(arr[ind] <= k){
+                    take = dp[ind - 1][k - arr[ind]];
+                }
+                dp[ind][k] = (not_take ||  take);
+            }
+        }
+        return dp[arr.length - 1][target];
+        //TC = O(n * k);
+        //SC = O(n * k);
     }
 }
