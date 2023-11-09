986f8a10eeb535ce28c502ab9755ef9d08d6a3fc
UNKNOWN <119309033+RAJ8664@users.noreply.github.com>
2023-11-09 22:10:21 +0530

    print_the_longest_common_subsequence.java

--- Dynamic_programming/DP_ON_STRINGS/print_the_longest_common_subsequence.java
+++ Dynamic_programming/DP_ON_STRINGS/print_the_longest_common_subsequence.java
@@ -5,47 +5,55 @@ public class print_the_longest_common_subsequence {
     public static void main(String[] args) {
         String s = "abcde";
         String t = "bdgek";
-        int ind1 = s.length();
-        int ind2 = t.length();
-        int dp[][] = new int[ind1 + 1][ind2  + 1];
-        for(int temp[] : dp){
-            Arrays.fill(temp , -1);
-        }
-        System.out.println(solve_tabu(ind1, ind2 , s, t, dp));
-
+        lcs(s,t);
     }
 
     //using tabulation;
-    public static String solve_tabu(int ind1 , int ind2, String s, String t ,int dp[][]){
-        for(int temp[] : dp){
-            Arrays.fill(temp , 0);
+   public static void lcs(String s1, String s2) {
+
+        int n=s1.length();
+        int m=s2.length();
+
+        int dp[][]=new int[n+1][m+1];
+        for(int i=0;i<=n;i++){
+            dp[i][0] = 0;
         }
-        for(int i = 1; i <= ind1; i++){
-            for(int j = 1; j <= ind2; j++){
-                if(s.charAt(i - 1) == t.charAt(j - 1)) {
-                    dp[i][j] = 1 +  dp[i - 1][j - 1];
-                }
-                else{
-                    dp[i][j] = Math.max(dp[i - 1][j] , dp[i][j - 1]);
-                }
+        for(int i=0;i<=m;i++){
+            dp[0][i] = 0;
+        }
+
+        for(int ind1=1;ind1<=n;ind1++){
+            for(int ind2=1;ind2<=m;ind2++){
+                if(s1.charAt(ind1-1)==s2.charAt(ind2-1))
+                    dp[ind1][ind2] = 1 + dp[ind1-1][ind2-1];
+                else
+                    dp[ind1][ind2] = 0 + Math.max(dp[ind1-1][ind2],dp[ind1][ind2-1]);
             }
         }
-        StringBuilder sb = new StringBuilder();
-        int i = ind1;
-        int j = ind2;
-        while(i > 0 && j > 0){
-            if(s.charAt(i - 1) == t.charAt(j - 1)){
-                sb.append(s.charAt(i - 1));
+
+        int len=dp[n][m];
+        int i=n;
+        int j=m;
+
+        int index = len-1;
+        String str="";
+        for(int k=1; k<=len;k++){
+            str +="$"; // dummy string
+        }
+        StringBuilder ss= new StringBuilder(s1);
+        StringBuilder str2=new StringBuilder(str);
+        while(i>0 && j>0){
+            if(ss.charAt(i-1) == s2.charAt(j-1)){
+                str2.setCharAt(index,ss.charAt(i-1) );
+                index--;
                 i--;
                 j--;
             }
-            else if(dp[i - 1][j] > dp[i][j - 1]){
+            else if(ss.charAt(i-1)>s2.charAt(j-1)){
                 i--;
             }
-            else{
-                j--;
-            }
+            else j--;
         }
-       return sb.reverse().toString();
+        System.out.println(str2);
     }
 }
